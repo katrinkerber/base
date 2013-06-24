e22df06deb9224bb1c668c93e5c3a8f303294e0b
Katrin Kerber <katrinkerber@gmail.com>
2013-06-24 18:24:17 +0100

    Deleted 'scripts-min.js'

--- js/scripts-min.js
+++
@@ -1,5 +0,0 @@
-/* 
-THIS FILE IS THE IMPORT AND MINIFY CONTROLLER SET IN CODEKIT
-ALL PLUGINS ARE IMPORTED TO THE TOP OF THIS FILE
-THE FILE IS THEN MINIMISED AND SAVED AS 'scripts-min.js'
-*//* ############## SET FUNCTIONS ############## *//* ############## CALL FUNCTIONS ############## */$(document).ready(function(){$("no-js").removeClass("no-js")});
