c07b24a1d8cc85fc049a76e9f05c5136310d9dc0
Katrin Kerber <katrinkerber@gmail.com>
2013-06-24 18:23:45 +0100

    'functions.js' to 'scripts.js'

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+/* THIS FILE IS THE IMPORT AND MINIFY CONTROLLER SET IN CODEKITALL PLUGINS ARE IMPORTED TO THE TOP OF THIS FILETHE FILE IS THEN MINIMISED AND SAVED AS 'scripts-ck.js'*//* ############## SET FUNCTIONS ############## *//* ############## CALL FUNCTIONS ############## */$(document).ready(function() {});
