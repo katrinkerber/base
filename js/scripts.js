01ebe6abe774fd67a5aac00fd75c0ebd6c7c577e
Katrin Kerber <katrinkerber@gmail.com>
2014-02-17 13:55:18 +0000

    line endings to unix

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-/* THIS FILE IS THE IMPORT AND MINIFY CONTROLLER SET IN CODEKITALL PLUGINS ARE IMPORTED TO THE TOP OF THIS FILETHE FILE IS THEN MINIMISED AND SAVED AS 'scripts.min.js'*//* ############## IMPORT PLUGINS ############## */// Does this project need jQuery? If we don't need <=IE8 support, use 2.x version? // Could we go Vanilla? (https://gist.github.com/liamcurry/2597326)// @codekit-prepend "libs/jquery-1.9.1.min.js";/* ############## SET FUNCTIONS ############## *//* ############## CALL FUNCTIONS ############## */$(document).ready(function() {});
+/* 
+THIS FILE IS THE IMPORT AND MINIFY CONTROLLER SET IN CODEKIT
+ALL PLUGINS ARE IMPORTED TO THE TOP OF THIS FILE
+THE FILE IS THEN MINIMISED AND SAVED AS 'scripts.min.js'
+*/
+
+/* ############## IMPORT PLUGINS ############## */
+// Does this project need jQuery? If we don't need <=IE8 support, use 2.x version? 
+// Could we go Vanilla? (https://gist.github.com/liamcurry/2597326)
+// @codekit-prepend "libs/jquery-1.9.1.min.js";
+
+
+/* ############## SET FUNCTIONS ############## */
+
+/* ############## CALL FUNCTIONS ############## */
+$(document).ready(function() {
+
+});
