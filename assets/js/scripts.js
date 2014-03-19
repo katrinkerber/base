83f879ad5f593380eb41bba6cb57649d0eea3bcb
Katrin Kerber <katrinkerber@gmail.com>
2014-03-19 09:33:44 +0000

    added mixture configs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,17 @@
 /* 
-THIS FILE IS THE IMPORT AND MINIFY CONTROLLER SET IN CODEKIT
-ALL PLUGINS ARE IMPORTED TO THE TOP OF THIS FILE
-THE FILE IS THEN MINIMISED AND SAVED AS 'scripts.min.js'
+THIS FILE IS MINIMISED AND SAVED AS 'scripts.min.js'
 */
 
 /* ############## IMPORT PLUGINS ############## */
 // Does this project need jQuery? If we don't need <=IE8 support, use 2.x version? 
 // Could we go Vanilla? (https://gist.github.com/liamcurry/2597326)
+
+// CodeKit import example:
 // @codekit-prepend "libs/jquery-1.9.1.min.js";
+
+// Mixture import example:
+// @import "libs/jquery-1.9.1.min.js";
+// If using Mixture, rename this file to scripts.mix.js to enable @import
 
 
 /* ############## SET FUNCTIONS ############## */
