066d6bc473c31f85506e2af2f57d661532b9c61b
Katrin Kerber <katrinkerber@gmail.com>
2014-09-22 10:53:05 +0100

    amended

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,14 +1,12 @@
 var _gaq = _gaq || [];
-  // Enhanced Link Attribution - https://support.google.com/analytics/answer/2558867?hl=en&utm_id=ad
-  var pluginUrl = 
-   '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
-  _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+// Enhanced Link Attribution - https://support.google.com/analytics/answer/2558867?hl=en&utm_id=ad
+var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+_gaq.push(['_setAccount', 'UA-XXXXX']);
+_gaq.push(['_trackPageview']);
 
-  _gaq.push(['_setAccount', 'UA-XXXXX-X']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function(d,t){
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g,s)
+}(document,'script'));
