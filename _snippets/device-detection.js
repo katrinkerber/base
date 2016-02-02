d5a5e98385bd8ad1cfd91967740b65d7964ead63
Katrin Kerber <katrinkerber@gmail.com>
2016-02-02 15:00:29 +0000

    created a _snippets folder

---
+++ _snippets/device-detection.js
@@ -0,0 +1,33 @@
+/* Device/OS detection - useful if wanting to check for things that modernizr doesn't check */
+
+// var ua = navigator.userAgent;
+// // <=iOS3
+// if( /(iPhone|iPod|iPad)/i.test(ua) ) {
+//     if(/OS [2-3]_\d(_\d)? like Mac OS X/i.test(ua)) {
+//         // iOS 2-3
+//         document.getElementsByTagName('html')[0].className += ' no-icons';
+//     }
+//     if(/OS [2-4]_\d(_\d)? like Mac OS X/i.test(ua)) {
+//         // iOS 2-4
+//         document.getElementsByTagName('html')[0].className += ' no-position-fixed';
+//     }
+// }
+// // Android <2
+// if( ua.indexOf("Android") >= 0 )
+// {
+//   var androidversion = parseFloat(ua.slice(ua.indexOf("Android")+8));
+//   if (androidversion < 2)
+//   {
+//       document.getElementsByTagName('html')[0].className += ' no-icons';
+//   }
+//   if (androidversion < 3)
+//   {
+//       document.getElementsByTagName('html')[0].className += ' no-position-fixed';
+//   }
+// }
+// // Opera Mini
+// if( ua.indexOf('Opera Mini') > -1) {
+//   document.getElementsByTagName('html')[0].className += ' no-icons no-position-fixed';
+// }
+
+// alert(document.getElementsByTagName('html')[0].className);
