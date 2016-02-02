e687e9597626c225d7f346ca7cffdcae97afb4d2
Katrin Kerber <katrinkerber@gmail.com>
2016-02-02 20:49:09 +0000

    more styleguide refactoring and amending

--- assets/styleguide/index.php
+++ assets/styleguide/index.php
@@ -13,7 +13,7 @@
   <meta name="robots" content="noindex, nofollow">
 
   <link rel="stylesheet" href="/assets/css/styles.min.css">
-  <link rel="stylesheet" href="/styleguide/styleguide.css">
+  <link rel="stylesheet" href="styleguide.css">
   <link rel="stylesheet" href="/assets/libs/prism/prism.css" />
 </head>
 
@@ -23,56 +23,38 @@
     <h1>{SITE NAME}</h1>
     <h2>Style Guide</h2>
     <nav>
-        <ul>
-          <!-- section links populated via JS -->
-        </ul>
+      <ul>
+        <!-- section links populated via JS -->
+      </ul>
     </nav>
   </header>
 
   <main role="main">
 
-    <section class="styleguide__section" id="landing">
-      <blockquote>
-          <p>Every line of code should appear to be written by a single person, no matter the number of contributors.</p>
-      </blockquote>
-
-      <h2>Basic editor preferences</h2>
-        <p>Set your editor to the following settings to avoid common code inconsistencies and dirty diffs:</p>
-      <ul>
-        <li>Use soft-tabs set to <strong>4 spaces</strong>.</li>
-        <li>Trim trailing white space on save.</li>
-        <li>Set encoding to UTF-8.</li>
-        <li>Add new line at end of files.</li>
-      </ul>
-
-      <p>If you are using <strong>Sublime Text 2</strong>, have a look at these <a href="https://gist.github.com/katrinkerber/d471b3f995f80f48117c">settings</a>.</p>
-
-      <p>
-        <small>This guide was heavily inspired by <a href="http://mdo.github.io/code-guide/">@mdo's Code Guide</a>.</small>
-      </p>
-    </section>
-
     <div data-section data-section-title="Coding guidelines">
-        <?php
-          include 'includes/notes_html.html';
-          include 'includes/notes_css.html';
-          include 'includes/notes_js.html';
-        ?>
+      <?php
+        include 'includes/notes/intro.html';
+        include 'includes/notes/html.html';
+        include 'includes/notes/css.html';
+        include 'includes/notes/js.html';
+      ?>
     </div>
 
     <div data-section data-section-title="Base">
-        <?php
-          include 'includes/colours.html';
-          include 'includes/typography.html';
-        ?>
+      <?php
+        include 'includes/base/colours.html';
+        include 'includes/base/typography.html';
+      ?>
     </div>
   </main>
-  <!-- /page -->
 
-<!-- JavaScript -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<!-- Styleguide specific -->
-<script src="/styleguide/prism/styleguide.js"></script>
-<script src="/styleguide/prism/prism.js"></script>
+
+  <!-- JavaScript -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="/assets/js/libs.min.js"></script>
+  <script src="/assets/js/global.min.js"></script>
+  <!-- Styleguide specific -->
+  <script src="styleguide.js"></script>
+  <script src="/assets/libs/prism/prism.js"></script>
 </body>
 </html>
