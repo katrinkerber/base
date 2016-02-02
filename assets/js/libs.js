28746478dad51133fc236008d939e1b8f9f9d75a
Katrin Kerber <katrinkerber@gmail.com>
2016-02-02 14:59:43 +0000

    created libs.js file which will house all third-party vendor scripts

---
+++ assets/js/libs.js
@@ -0,0 +1,11 @@
+/*
+THIS FILE IS MINIMISED AND SAVED AS 'libs.min.js'
+*/
+
+/* ############## IMPORT/ADD LIBS ############## */
+// CodeKit import example:
+// @codekit-prepend "libs/jquery-1.9.1.min.js";
+
+// Mixture import example:
+// @import "libs/jquery-1.9.1.min.js";
+// If using Mixture, rename this file to global.mix.js to enable imports
