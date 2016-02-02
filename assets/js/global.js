c59d0e49607968a2387fef46dbcf9a3ce5fb5be0
Katrin Kerber <katrinkerber@gmail.com>
2016-02-02 14:59:55 +0000

    global.js no longer imports third-party scripts

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,18 +2,6 @@
 THIS FILE IS MINIMISED AND SAVED AS 'global.min.js'
 */
 
-/* ############## IMPORT PLUGINS ############## */
-// Does this project need jQuery? If we don't need <=IE8 support, use 2.x version?
-// Could we go Vanilla? (https://gist.github.com/liamcurry/2597326)
-
-// CodeKit import example:
-// @codekit-prepend "libs/jquery-1.9.1.min.js";
-
-// Mixture import example:
-// @import "libs/jquery-1.9.1.min.js";
-// If using Mixture, rename this file to scripts.mix.js to enable @import
-
-
 /* ############## SET FUNCTIONS ############## */
 
 
