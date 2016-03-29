e18857db137394f88c7d412565e305fb9f115cbc
Katrin Kerber <katrinkerber@gmail.com>
2016-03-29 20:50:40 +0100

    tweaks to global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,10 +2,15 @@
 THIS FILE IS MINIMISED AND SAVED AS 'global.min.js'
 */
 
+/* ############## IMPORTED LIBRARIES ############## */
+// CodeKit imports:
+// @codekit-prepend "/assets/libs/jquery.validate.min.js";
+
+// Mixture import example:
+// @import "libs/jquery-1.9.1.min.js";
+// If using Mixture, rename this file to global.mix.js to enable imports
+
 /* ############## SET FUNCTIONS ############## */
 
 
 /* ############## CALL FUNCTIONS ############## */
-$(document).ready(function() {
-
-});
